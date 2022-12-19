6ab6e77146d4ccea3f17ae0d01031d263d9bcfc5
13195 <ryan.miller@vtsd.com>
2022-12-19 07:57:10 -0500

    Chapter 11 notes

---
+++ FirstArray/Example.java
@@ -0,0 +1,22 @@
+package FirstArray;
+public class Example {
+    public static void main(String args[]){
+
+        //Arrays store one datatype
+        int numberOfChristmasGifts[];
+        numberOfChristmasGifts = new int[2];
+
+        numberOfChristmasGifts[0] = 7;
+        numberOfChristmasGifts[1] = 3;
+
+        System.out.println(numberOfChristmasGifts[0]);
+        System.out.println(numberOfChristmasGifts[1]);
+
+        numberOfChristmasGifts[0] = 42;
+        numberOfChristmasGifts[1] = 6;
+
+        System.out.println(numberOfChristmasGifts[0]);
+        System.out.println(numberOfChristmasGifts[1]);
+    }
+
+}
